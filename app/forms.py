e0b52ab605a03c55a03f81cf5a2e91ec534ceb49
Romario Lawes <romario.lawes@mymona.uwi.edu>
2018-02-22 01:21:15 +0000

    Exercise 1

---
+++ app/forms.py
@@ -0,0 +1,5 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
+class UploadForm(FlaskForm):
+    image = FileField('Image', validators = [FileRequired(), FileAllowed(['jpg', 'png'])])
