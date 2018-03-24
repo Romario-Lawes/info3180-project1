d45f59a55e08dc239829636e8e065294f6623c4f
Romario Lawes <romario.lawes@mymona.uwi.edu>
2018-03-24 21:30:55 +0000

    Heroku URI and Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ UPLOAD_FOLDER = './app/static/uploads'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "$3KRET"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://mwneuvcncwhzor:2990fb67f2ac8546943008e253a025c0f5acbcc2929977ac912dd653b1a84011@ec2-54-235-146-51.compute-1.amazonaws.com:5432/d9cvq1m78bnaor"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
 
 db = SQLAlchemy(app)
